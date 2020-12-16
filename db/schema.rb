c2555d8754f4e4e4ea9a6742f4662b6f85f0b3ee
AJavier55 <aliciajavier5@gmail.com>
2020-12-16 16:35:47 -0500

    fixed items table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_16_164407) do
+ActiveRecord::Schema.define(version: 2020_12_16_213248) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,11 +40,12 @@ ActiveRecord::Schema.define(version: 2020_12_16_164407) do
     t.string "name"
     t.string "image"
     t.string "description"
-    t.string "price"
     t.string "category"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
+    t.integer "price"
+    t.integer "quantity"
   end
 
   create_table "purchases", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Item.delete_all
+User.delete_all
+Purchase.delete_all
+
+Item.create(name: "1900s Edwardian Gibson", image: "https://thepragmaticcostumer.files.wordpress.com/2012/03/dressforsale.jpg?w=336&h=240", description: "This is in amazing condition! There are a couple tiny breaks in lace that you really have to search for but no holes or stains! Could be worn with caution. Bodice measures 15” across the shoulders, 40” through the bust, 25” at the waist, and length is 20”. Sleeves are 22” long from shoulder seam to cuff. Length from top of shoulder to waist seam is 17”. Bodice has high neck, lace inserts, floating lace panels around yoke, pigeon breast and full sleeves with fitted lace cuff", category,  )
+
