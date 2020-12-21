7c592dab2b07b489745c7afe7c98b91f213f81c8
AJavier55 <aliciajavier5@gmail.com>
2020-12-21 13:16:13 -0500

    done with mvp

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,4 +1,4 @@
-class Api::V1::PurchasesController < ApplicationController
+class PurchasesController < ApplicationController
 
     def index
         purchases = Purchase.all
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
+      resources :items
+      resources :purchases
       namespace :api do 
             namespace :v1 do
                   resources :users, only: [:create]
-                  resources :items
-                  resources :purchases
                   # get '/items', to: 'users#items'
                   post "/login", to: "auth#create"
                   get "/profile", to: "users#profile"
